5b918fefcfc2a769f54642bea3960eed1a8c39fe
Anzo52 <andy@zollnersolutions.com>
2022-10-31 02:24:34 -0600

    testing

--- src/main/java/com/zollnersolutions/App.java
+++ src/main/java/com/zollnersolutions/App.java
@@ -1,11 +1,32 @@
 package com.zollnersolutions;
 
-
+import com.fasterxml.jackson.databind.JsonNode;
 import java.io.IOException;
+import org.json.JSONObject;
 
 public final class App {
     public static void main(String[] args) throws IOException {
-        String userQuery = args[0];
-        String openlibQUrl = "http://openlibrary.org/search.json?q=%s";
+        //String userQuery = args[0];
+        String userQuery = "moby dick";
+        String searchString;
+        if (userQuery.length() > 1) {
+            searchString = userQuery.replaceAll(" ", "+");
+        } else {
+            searchString = userQuery;
+        }
+        String openlibUrl = "http://openlibrary.org/%s";
+        String openlibSearchUrl = String.format("http://openlibrary.org/search.json?q=%s", searchString);
+        String booksUrl = String.format(openlibUrl, "books");
+        String authorsUrl = String.format(openlibUrl, "authors");
+        String worksUrl = String.format(openlibUrl, "works");
+        // use "search.json?q=" for ISBN
+
+        String jstream = JsonUrlReader.stream(openlibSearchUrl);
+        String jstring = JsonUrlReader.getString(openlibSearchUrl);
+        JSONObject jobj = JsonUrlReader.getJson(openlibSearchUrl);
+        JsonNode node = JsonUrlReader.get(openlibSearchUrl);
+
+        System.out.println(jobj);
+
     }
 }
